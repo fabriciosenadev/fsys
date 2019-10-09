3564954e3ae410dcaff8bb720deb950acea4ffbf
Fabricio <fabricior.sena@outlook.com>
2019-10-08 22:20:14 -0300

    app structure reordered

---
+++ resources/template/app/footer.php
@@ -0,0 +1,14 @@
+    <!-- <footer>
+        <p>&copy; 2019 Fsys - Todos os direitos reservados
+            <br>
+            Termos de uso e Política de privacidade
+        </p>
+    </footer> -->
+
+    <!-- Arquivo do JQuery -->
+    <script type='text/javascript' src="../resources/assets/js/jquery-3.3.1.min.js"></script>
+    <!-- Arquivo do bootstrap 4.3.1-->
+    <script type='text/javascript' src="../resources/assets/js/bootstrap.bundle.min.js"></script>
+</body>
+
+</html>
---
+++ resources/template/app/header.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <!-- determina a largunra do site -->
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+
+    <!-- CSS do Bootstrap 4.3.1 -->
+    <link rel="stylesheet" href="../resources/assets/css/bootstrap.css">
+    <link rel="stylesheet" href="../resources/assets/css/home.css">
+    <title>fsys</title>
+</head>
+
+<body>
+    <!-- inicio container  -->
+    <div class='container-fluid'>
+
+        <!-- inicio top menu -->
+        <?php require_once '../resources/views/app/menu.view.php';?>
+        <!-- fim top menu -->
+    </div>
