e18c2f499aef84d8d30cade6f5a2840fd0d3cddb
Fabricio <fabricior.sena@outlook.com>
2019-10-08 22:23:41 -0300

    home from site renamed to index

--- index.php
+++ index.php
@@ -6,7 +6,7 @@ $error = isset($error) ? $error : null;
 
 if(!($error))
 {
-    include 'resources/views/site/home.view.php'; 
+    include 'resources/views/site/index.view.php'; 
 }
 
 require_once 'resources/template/site/footer.php';
