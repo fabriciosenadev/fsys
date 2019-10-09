0d758be03b28805e682d2e9eb1c269c1f0e5cb12
Fabricio <fabricior.sena@outlook.com>
2019-10-08 23:58:41 -0300

    start to prepare login form

--- index.php
+++ index.php
@@ -1,12 +1,40 @@
 <?php
 
+// header do site ==> não alterar
 require_once 'resources/template/site/header.php';
 
-$error = isset($error) ? $error : null;
+require_once 'db/connect.php';
+require_once "model/site/index.model.php";
 
-if(!($error))
-{
-    include 'resources/views/site/index.view.php'; 
+
+// controller
+session_start();
+$login              = isset($_POST['login']) ? $_POST['login'] : null;
+$password           = isset($_POST['password']) ? $_POST['password'] : null;
+$btnConnect         = isset($_POST['btnConnect']) ? $_POST['btnConnect'] : null;
+$_SESSION['erros']  = [];
+
+function search($login) {
+   $result = 0 ;
 }
 
+function verify($login, $password){
+
+}
+
+if($btnConnect){
+    if(empty($login) or empty($password)) {
+        //TODO: envia erros para exibir
+        $_SESSION['erros'][] = "erro";
+    } else {
+        
+    }
+}
+if(!($errors)){    
+    include 'resources/views/site/index.view.php'; 
+}else{
+    header("Location: login.php");
+}
+
+// footer do site ==> não alterar
 require_once 'resources/template/site/footer.php';
