2343bffb306e3246468ca176e23444fd720df998
Fabricio Sena <fabricior.sena@outlook.com>
2019-10-16 16:39:18 -0300

    method to create database created

--- db/connect.php
+++ db/connect.php
@@ -1,19 +1,30 @@
 <?php
 $db_server = "localhost";
 $db_user = "fabricio";
-$db_pass = "2um0n3ry";
+$db_pass = "Con2309r!";
 $db_name = "fsys";
+
+$query = '';
 
 $connection = mysqli_connect($db_server, $db_user, $db_pass, $db_name);
 
-//
+// erros
 $error = "Falha na conexao: ".mysqli_connect_error();
+$status = "Unknown";
 
 if($error){
     // echo $error;
     $arrError = explode(" ",$error);
-    if(in_array("Unknown",$arrError)){
-        var_dump($arrError);
+    if(in_array($status, $arrError)){
+        // var_dump($arrError);
+
+        $connection = mysqli_connect($db_server, $db_user, $db_pass);
+
+        $sql = "CREATE DATABASE fsys";
+        $check = mysqli_query($connection, $sql);
+        if($check) {
+            echo "It was created successfully";
+        }
         // https://www.w3schools.com/php/php_mysql_create.asp
         // usar para criar banco de dados
     }
