53eb323840fa452c2889329175519f6a2f7c8a03
Fabricio Sena <fabricior.sena@outlook.com>
2019-10-21 17:37:47 -0300

    error treatment added

--- app/cash_out.php
+++ app/cash_out.php
@@ -12,32 +12,90 @@ if (!$_SESSION['logged']) {
     header("Location: ../login.php");
 }
 
-
-// echo "<pre>";
-// var_dump($_REQUEST);
-// echo "</pre>";
-
 // fields
 $date = isset($_REQUEST['date']) ? $_REQUEST['date'] : null;
 $category = isset($_REQUEST['category']) ? $_REQUEST['category'] : null;
 $description = isset($_REQUEST['description']) ? $_REQUEST['description'] : null;
+$value = isset($_REQUEST['value']) ? floatval($_REQUEST['value']) : null;
 $payMethod = isset($_REQUEST['payMethod']) ? $_REQUEST['payMethod'] : null;
-$value = isset($_REQUEST['value']) ? $_REQUEST['value'] : null;
-
 $btnSave = isset($_REQUEST['btnSave']) ? $_REQUEST : null;
 
+$_SESSION['errors'] = [];
+$styleDate = $styleCategory = $styleValue = '';
+$reload = false;
+// echo "<pre>";
+// var_dump($_REQUEST);
+// var_dump($_SESSION['errors']);
+// echo "</pre>";
 
-// TODO: criar erros
 
+
+
+//TODO: criar metodo de gravação dos dados do lançamento
+if ($btnSave) {
+
+        // TODO: criar erros
+    if (empty($date)) {
+        $_SESSION['errors']['date'] ="Preencha a data."; 
+        $styleDate = 'is-invalid'; 
+    } 
+
+    if (empty($category)) {
+        $_SESSION['errors']['category'] = "Escolha uma categoria.";
+        $styleCategory = 'is-invalid';    
+    } 
+
+    if(empty($value)){
+        $_SESSION['errors']['value'] = "Preencha o valor.";
+        $styleValue = 'is-invalid';    
+    } else if($value <= 0.00) {
+        $_SESSION['errors']['value'] = "Valor muito baixo.";
+        $styleValue = 'is-invalid';
+    }
+
+    if (empty($payMethod)) {        
+        $_SESSION['errors']['payMethod'] = "Escolha uma forma de pagamento.";
+        $stylePayMethod = 'is-invalid';    
+    } 
+
+    if (count($_SESSION['errors']) > 0 && !(empty($date))){
+        $styleDate = 'is-valid';
+    }
+
+    if (count($_SESSION['errors']) > 0 && !(empty($category))){
+        $styleCategory = 'is-valid';
+    }
+
+    if (count($_SESSION['errors']) > 0 && !(empty($value))){
+        $styleValue = 'is-valid';
+    }
+
+    if (count($_SESSION['errors']) > 0 && !(empty($payMethod))){
+        $stylePayMethod = 'is-valid';
+    }
+
+    // valida descrição enviada pelo usuário
+    if ($description) {
+        $description = filter_var($description, FILTER_SANITIZE_STRING);
+    }
+
+
+
+    // if ($_SESSION['errors']) {
+    //     $reload = true;
+    // }
+}
+
+
+// redirecionamentos
+if($reload){
+    // header("Location: cash_in.php");
+}
 
 
 //TODO: criar metodo de retorno de dados dos campos category e payMethod
 $categories = selectCategories('OUT');    
 $payMethods = selectPayMethod();
-
-//TODO: criar metodo de gravação dos dados do lançamento
-
-
 
 include '../resources/views/app/launch.form.view.php'; 
 
