453dcd6a4fb7e0d5241abc933bdeb508b44f5a51
Fabricio Sena <fabricior.sena@outlook.com>
2019-10-22 14:09:18 -0300

    add new relation table to payMethod

--- db/fsys.sql
+++ db/fsys.sql
@@ -1,5 +1,5 @@
 -- --------------------------------------------------------
--- Servidor:                     localhost
+-- Servidor:                     127.0.0.1
 -- Versão do servidor:           5.7.27-0ubuntu0.18.04.1 - (Ubuntu)
 -- OS do Servidor:               Linux
 -- HeidiSQL Versão:              10.1.0.5464
@@ -13,12 +13,10 @@
 
 
 -- Copiando estrutura do banco de dados para fsys
-DROP DATABASE IF EXISTS `fsys`;
 CREATE DATABASE IF NOT EXISTS `fsys` /*!40100 DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci */;
 USE `fsys`;
 
 -- Copiando estrutura para tabela fsys.categories
-DROP TABLE IF EXISTS `categories`;
 CREATE TABLE IF NOT EXISTS `categories` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `category` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
@@ -32,25 +30,20 @@ CREATE TABLE IF NOT EXISTS `categories` (
   CONSTRAINT `fk_categories_created_by` FOREIGN KEY (`created_by`) REFERENCES `users` (`id`) ON DELETE CASCADE
 ) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
--- Copiando dados para a tabela fsys.categories: ~2 rows (aproximadamente)
-DELETE FROM `categories`;
+-- Copiando dados para a tabela fsys.categories: ~3 rows (aproximadamente)
 /*!40000 ALTER TABLE `categories` DISABLE KEYS */;
 INSERT INTO `categories` (`id`, `category`, `applicable`, `created_by`, `created_at`, `updated_at`, `deleted_at`) VALUES
-	(1, 'Salario', 'IN', NULL, NULL, NULL, NULL);
-INSERT INTO `categories` (`id`, `category`, `applicable`, `created_by`, `created_at`, `updated_at`, `deleted_at`) VALUES
-	(2, 'Transporte', 'OUT', NULL, NULL, NULL, NULL);
-INSERT INTO `categories` (`id`, `category`, `applicable`, `created_by`, `created_at`, `updated_at`, `deleted_at`) VALUES
+	(1, 'Salario', 'IN', NULL, NULL, NULL, NULL),
+	(2, 'Transporte', 'OUT', NULL, NULL, NULL, NULL),
 	(3, 'Supermercado', 'OUT', NULL, NULL, NULL, NULL);
 /*!40000 ALTER TABLE `categories` ENABLE KEYS */;
 
 -- Copiando estrutura para tabela fsys.historics
-DROP TABLE IF EXISTS `historics`;
 CREATE TABLE IF NOT EXISTS `historics` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `date` timestamp NULL DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP,
   `id_category` int(11) NOT NULL,
   `description` varchar(250) COLLATE utf8_unicode_ci NOT NULL,
-  `id_pay_method` int(11) NOT NULL,
   `value` decimal(8,2) NOT NULL,
   `created_by` int(11) DEFAULT NULL,
   `created_at` timestamp NULL DEFAULT NULL,
@@ -58,20 +51,38 @@ CREATE TABLE IF NOT EXISTS `historics` (
   `deleted_at` timestamp NULL DEFAULT NULL,
   PRIMARY KEY (`id`),
   KEY `fk_historics_created_by` (`created_by`),
-  KEY `fk_historics_id_pay_method` (`id_pay_method`),
   KEY `fk_historics_id_category` (`id_category`),
   CONSTRAINT `fk_historics_created_by` FOREIGN KEY (`created_by`) REFERENCES `users` (`id`) ON DELETE CASCADE,
-  CONSTRAINT `fk_historics_id_category` FOREIGN KEY (`id_category`) REFERENCES `categories` (`id`) ON DELETE CASCADE,
-  CONSTRAINT `fk_historics_id_pay_method` FOREIGN KEY (`id_pay_method`) REFERENCES `pay_methods` (`id`) ON DELETE CASCADE
+  CONSTRAINT `fk_historics_id_category` FOREIGN KEY (`id_category`) REFERENCES `categories` (`id`) ON DELETE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 -- Copiando dados para a tabela fsys.historics: ~0 rows (aproximadamente)
-DELETE FROM `historics`;
 /*!40000 ALTER TABLE `historics` DISABLE KEYS */;
 /*!40000 ALTER TABLE `historics` ENABLE KEYS */;
 
+-- Copiando estrutura para tabela fsys.historic_pay_methods
+CREATE TABLE IF NOT EXISTS `historic_pay_methods` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `id_historic` int(11) NOT NULL DEFAULT '0',
+  `id_pay_method` int(11) NOT NULL DEFAULT '0',
+  `created_by` int(11) NOT NULL DEFAULT '0',
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL,
+  `deleted_at` timestamp NULL DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `fk_historic_id_pay_methods_historics` (`id_historic`),
+  KEY `fk_historic_id_pay_methods_pay_methods` (`id_pay_method`),
+  KEY `fk_historic_id_pay_methods_user` (`created_by`),
+  CONSTRAINT `fk_historic_id_pay_methods_historics` FOREIGN KEY (`id_historic`) REFERENCES `historics` (`id`) ON DELETE CASCADE,
+  CONSTRAINT `fk_historic_id_pay_methods_pay_methods` FOREIGN KEY (`id_pay_method`) REFERENCES `pay_methods` (`id`),
+  CONSTRAINT `fk_historic_id_pay_methods_user` FOREIGN KEY (`created_by`) REFERENCES `users` (`id`) ON DELETE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci COMMENT='relation between historics and pay_methods';
+
+-- Copiando dados para a tabela fsys.historic_pay_methods: ~0 rows (aproximadamente)
+/*!40000 ALTER TABLE `historic_pay_methods` DISABLE KEYS */;
+/*!40000 ALTER TABLE `historic_pay_methods` ENABLE KEYS */;
+
 -- Copiando estrutura para tabela fsys.pay_methods
-DROP TABLE IF EXISTS `pay_methods`;
 CREATE TABLE IF NOT EXISTS `pay_methods` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `pay_method` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
@@ -85,19 +96,15 @@ CREATE TABLE IF NOT EXISTS `pay_methods` (
   CONSTRAINT `fk_pay_method_created_by` FOREIGN KEY (`created_by`) REFERENCES `users` (`id`) ON DELETE CASCADE
 ) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
--- Copiando dados para a tabela fsys.pay_methods: ~2 rows (aproximadamente)
-DELETE FROM `pay_methods`;
+-- Copiando dados para a tabela fsys.pay_methods: ~3 rows (aproximadamente)
 /*!40000 ALTER TABLE `pay_methods` DISABLE KEYS */;
 INSERT INTO `pay_methods` (`id`, `pay_method`, `applicable`, `created_by`, `created_at`, `updated_at`, `deleted_at`) VALUES
-	(1, 'Dinheiro', 'WALLET', NULL, NULL, NULL, NULL);
-INSERT INTO `pay_methods` (`id`, `pay_method`, `applicable`, `created_by`, `created_at`, `updated_at`, `deleted_at`) VALUES
-	(2, 'Debito', 'ACCOUNT', NULL, NULL, NULL, NULL);
-INSERT INTO `pay_methods` (`id`, `pay_method`, `applicable`, `created_by`, `created_at`, `updated_at`, `deleted_at`) VALUES
+	(1, 'Dinheiro', 'WALLET', NULL, NULL, NULL, NULL),
+	(2, 'Debito', 'ACCOUNT', NULL, NULL, NULL, NULL),
 	(3, 'Credito', 'CREDIT', NULL, NULL, NULL, NULL);
 /*!40000 ALTER TABLE `pay_methods` ENABLE KEYS */;
 
 -- Copiando estrutura para tabela fsys.users
-DROP TABLE IF EXISTS `users`;
 CREATE TABLE IF NOT EXISTS `users` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `name` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
@@ -109,8 +116,7 @@ CREATE TABLE IF NOT EXISTS `users` (
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
--- Copiando dados para a tabela fsys.users: ~0 rows (aproximadamente)
-DELETE FROM `users`;
+-- Copiando dados para a tabela fsys.users: ~1 rows (aproximadamente)
 /*!40000 ALTER TABLE `users` DISABLE KEYS */;
 INSERT INTO `users` (`id`, `name`, `email`, `password`, `created_at`, `updated_at`, `deleted_at`) VALUES
 	(1, 'teste', 'teste@teste.com', '123', NULL, NULL, NULL);
