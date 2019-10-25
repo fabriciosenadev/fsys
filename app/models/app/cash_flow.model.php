de2bd5cd17e09d992e95250f9c1fce3d03c755fb
Fabricio <fabricior.sena@outlook.com>
2019-10-24 23:47:08 -0300

    add unified file to create launchment

---
+++ app/models/app/cash_flow.model.php
@@ -0,0 +1,73 @@
+<?php
+
+// criação das definições do banco de dados
+require '../db/connect.php';
+
+/**
+ *  function saveIn
+ *  @param array $data
+ *  @return int|boolean
+ */
+function saveLaunch($data) 
+{
+    $createHistoricReturn = createHistoric($data);
+
+    if ($createHistoricReturn && $data['id_pay_method']) {
+        $data['id_historic'] = $createHistoricReturn;
+
+        $savePayMethodOutReturn = savePayMethodOut($data);
+
+        return $savePayMethodOutReturn;
+    } else {
+
+        return $createHistoricReturn;
+
+    }
+
+
+}
+
+
+/**
+ * function createHistoric
+ * @param array $data
+ * @return int|boolean
+ */
+function createHistoric($data)
+{
+    $connection = $GLOBALS['connection'];
+
+    extract($data);
+    
+    $insert = "INSERT INTO historics(date, id_category, description, value, created_by, created_at)";
+    $insert .= "VALUES('$date', $id_category, '$description', $value, $created_by, now() )";
+
+    if (mysqli_query($connection, $insert) ) {
+        $IdHistoric = mysqli_insert_id($connection);
+    }
+
+    return $IdHistoric;
+
+}
+
+/**
+ * function savePayMethodOut
+ * @param array $data
+ * @return int|boolean
+ */
+function savePayMethodOut($data)
+{
+    $connection = $GLOBALS['connection'];
+
+    extract($data);
+
+    $insert = "INSERT INTO pay_method_historics(id_historic, id_pay_method, created_by, created_at)";
+    $insert .= "VALUES($id_historic, $id_pay_method, $created_by, now() )";
+
+    if (mysqli_query($connection, $insert) ) {
+        $IdPayMethodHistoric = mysqli_insert_id($connection);
+    }
+
+    return $IdPayMethodHistoric;
+
+}
