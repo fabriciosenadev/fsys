f2a47012e17c65568ff9177482b13ad6cf756bbb
Fabricio Sena <fabricior.sena@outlook.com>
2020-02-11 23:55:23 -0300

    start to create style to dashboard

--- resources/views/app/index.view.php
+++ resources/views/app/index.view.php
@@ -14,15 +14,78 @@
                     <?php
                         // include 'launch.form.view.php';
                     ?>
+
                     <h1>Main Area</h1>
-                    <p>Sriracha biodiesel taxidermy organic post-ironic, Intelligentsia salvia mustache 90's code editing brunch. Butcher polaroid VHS art party, hashtag Brooklyn deep v PBR narwhal sustainable mixtape swag wolf squid tote bag. Tote bag cronut semiotics,
+                    <div class="card-deck">
+                        <div class="card">
+                        <div class="card-header">
+                            <h5 class="card-title">Mês</h5>
+                            </div>
+                            <div class="card-body">
+                            <ul class="list-group list-group-flush">
+                                <li class="list-group-item">Entrada</li>
+                                <li class="list-group-item">Pendente Entrada</li>
+                                <li class="list-group-item">Saída</li>
+                                <li class="list-group-item">Pendente Saída</li>
+                                <!-- <li class="list-group-item">Cras justo odio</li>
+                                <li class="list-group-item">Dapibus ac facilisis in</li>
+                                <li class="list-group-item">Vestibulum at eros</li> -->
+                            </ul>
+                            <!-- <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p> -->
+                            <!-- <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p> -->
+                            </div>
+
+                        </div>
+                        <div class="card">
+                            
+                            <div class="card-body">
+                            <h5 class="card-title">Mês</h5>
+                            <ul class="list-group list-group-flush">
+                                <li class="list-group-item">Entrada</li>
+                                <li class="list-group-item">Pendente Entrada</li>
+                                <li class="list-group-item">Saída</li>
+                                <li class="list-group-item">Pendente Saída</li>
+                                <!-- <li class="list-group-item">Vestibulum at eros</li> -->
+                            </ul>
+                            <!-- <p class="card-text">This card has supporting text below as a natural lead-in to additional content.</p> -->
+                            <!-- <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p> -->
+                            </div>
+                        </div>
+                        <div class="card">
+                            <div class="card-header">
+                            Mês
+                            <!-- <h5 class="card-title">Card title</h5> -->
+                            </div>
+                            <div class="card-body">
+                            <ul class="list-group list-group-flush">
+                                <li class="list-group-item">Entrada</li>
+                                <li class="list-group-item">Pendente Entrada</li>
+                                <li class="list-group-item">Saída</li>
+                                <li class="list-group-item">Pendente Saída</li>
+                                <!-- <li class="list-group-item">Cras justo odio</li>
+                                <li class="list-group-item">Dapibus ac facilisis in</li>
+                                <li class="list-group-item">Vestibulum at eros</li> -->
+                            </ul>
+                            <!-- <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p> -->
+                            <!-- <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p> -->
+                            </div>
+                        </div>
+                    </div>                    
+
+
+
+
+                    
+                    <!-- <p>Sriracha biodiesel taxidermy organic post-ironic, Intelligentsia salvia mustache 90's code editing brunch. Butcher polaroid VHS art party, hashtag Brooklyn deep v PBR narwhal sustainable mixtape swag wolf squid tote bag. Tote bag cronut semiotics,
                         raw denim deep v taxidermy messenger bag. Tofu YOLO Etsy, direct trade ethical Odd Future jean shorts paleo. Forage Shoreditch tousled aesthetic irony, street art organic Bushwick artisan cliche semiotics ugh synth chillwave meditation.
                         Shabby chic lomo plaid vinyl chambray Vice. Vice sustainable cardigan, Williamsburg master cleanse hella DIY 90's blog.</p>
 
                     <p>Ethical Kickstarter PBR asymmetrical lo-fi. Dreamcatcher street art Carles, stumptown gluten-free Kickstarter artisan Wes Anderson wolf pug. Godard sustainable you probably haven't heard of them, vegan farm-to-table Williamsburg slow-carb
                         readymade disrupt deep v. Meggings seitan Wes Anderson semiotics, cliche American Apparel whatever. Helvetica cray plaid, vegan brunch Banksy leggings +1 direct trade. Wayfarers codeply PBR selfies. Banh mi McSweeney's Shoreditch selfies,
-                        forage fingerstache food truck occupy YOLO Pitchfork fixie iPhone fanny pack art party Portland.</p>
-                        
+                        forage fingerstache food truck occupy YOLO Pitchfork fixie iPhone fanny pack art party Portland.</p> -->
+
+
+
                     <!-- <p>Sriracha biodiesel taxidermy organic post-ironic, Intelligentsia salvia mustache 90's code editing brunch. Butcher polaroid VHS art party, hashtag Brooklyn deep v PBR narwhal sustainable mixtape swag wolf squid tote bag. Tote bag cronut semiotics,
                         raw denim deep v taxidermy messenger bag. Tofu YOLO Etsy, direct trade ethical Odd Future jean shorts paleo. Forage Shoreditch tousled aesthetic irony, street art organic Bushwick artisan cliche semiotics ugh synth chillwave meditation.
                         Shabby chic lomo plaid vinyl chambray Vice. Vice sustainable cardigan, Williamsburg master cleanse hella DIY 90's blog.</p> -->
