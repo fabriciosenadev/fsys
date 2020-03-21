893177f4c24924cf9e547b4ee7a316579f872e40
Fabricio Sena <fabricior.sena@outlook.com>
2020-03-21 16:45:14 -0300

    saparete descriptions from historic table

--- db/fsys.sql
+++ db/fsys.sql
@@ -30,9 +30,9 @@ CREATE TABLE IF NOT EXISTS `categories` (
   PRIMARY KEY (`id`),
   KEY `fk_categories_created_by` (`created_by`),
   CONSTRAINT `fk_categories_created_by` FOREIGN KEY (`created_by`) REFERENCES `users` (`id`) ON DELETE CASCADE
-) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
--- Copiando dados para a tabela fsys.categories: ~7 rows (aproximadamente)
+-- Copiando dados para a tabela fsys.categories: ~0 rows (aproximadamente)
 DELETE FROM `categories`;
 /*!40000 ALTER TABLE `categories` DISABLE KEYS */;
 /*!40000 ALTER TABLE `categories` ENABLE KEYS */;
@@ -54,19 +54,38 @@ CREATE TABLE IF NOT EXISTS `category_users` (
   CONSTRAINT `fk_category_users_created_by` FOREIGN KEY (`created_by`) REFERENCES `users` (`id`) ON DELETE CASCADE,
   CONSTRAINT `fk_category_users_id_category` FOREIGN KEY (`id_category`) REFERENCES `categories` (`id`) ON DELETE CASCADE,
   CONSTRAINT `fk_category_users_id_user` FOREIGN KEY (`id_user`) REFERENCES `users` (`id`) ON DELETE CASCADE
-) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
--- Copiando dados para a tabela fsys.category_users: ~7 rows (aproximadamente)
+-- Copiando dados para a tabela fsys.category_users: ~0 rows (aproximadamente)
 DELETE FROM `category_users`;
 /*!40000 ALTER TABLE `category_users` DISABLE KEYS */;
 /*!40000 ALTER TABLE `category_users` ENABLE KEYS */;
+
+-- Copiando estrutura para tabela fsys.descriptions
+DROP TABLE IF EXISTS `descriptions`;
+CREATE TABLE IF NOT EXISTS `descriptions` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `description` varchar(255) DEFAULT NULL,
+  `created_by` int(11) DEFAULT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL,
+  `deleted_at` timestamp NULL DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `fk_descriptions_id_user` (`created_by`),
+  CONSTRAINT `fk_descriptions_id_user` FOREIGN KEY (`created_by`) REFERENCES `users` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- Copiando dados para a tabela fsys.descriptions: ~0 rows (aproximadamente)
+DELETE FROM `descriptions`;
+/*!40000 ALTER TABLE `descriptions` DISABLE KEYS */;
+/*!40000 ALTER TABLE `descriptions` ENABLE KEYS */;
 
 -- Copiando estrutura para tabela fsys.historics
 DROP TABLE IF EXISTS `historics`;
 CREATE TABLE IF NOT EXISTS `historics` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `date` date DEFAULT NULL,
-  `description` varchar(250) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `id_description` int(11) DEFAULT NULL,
   `status` enum('PAID','RECEIVED','PENDING') COLLATE utf8_unicode_ci NOT NULL,
   `id_category` int(11) DEFAULT NULL,
   `id_value` int(11) DEFAULT NULL,
@@ -78,12 +97,14 @@ CREATE TABLE IF NOT EXISTS `historics` (
   KEY `fk_historics_created_by` (`created_by`),
   KEY `fk_historics_id_category` (`id_category`),
   KEY `fk_historics_id_values` (`id_value`),
+  KEY `fk_historics_id_description` (`id_description`),
   CONSTRAINT `fk_historics_created_by` FOREIGN KEY (`created_by`) REFERENCES `users` (`id`) ON DELETE CASCADE,
   CONSTRAINT `fk_historics_id_category` FOREIGN KEY (`id_category`) REFERENCES `categories` (`id`) ON DELETE CASCADE,
+  CONSTRAINT `fk_historics_id_description` FOREIGN KEY (`id_description`) REFERENCES `descriptions` (`id`),
   CONSTRAINT `fk_historics_id_values` FOREIGN KEY (`id_value`) REFERENCES `values` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
--- Copiando dados para a tabela fsys.historics: ~4 rows (aproximadamente)
+-- Copiando dados para a tabela fsys.historics: ~0 rows (aproximadamente)
 DELETE FROM `historics`;
 /*!40000 ALTER TABLE `historics` DISABLE KEYS */;
 /*!40000 ALTER TABLE `historics` ENABLE KEYS */;
@@ -161,9 +182,9 @@ CREATE TABLE IF NOT EXISTS `pay_method_historics` (
   CONSTRAINT `fk_pay_method_historics_id_historics` FOREIGN KEY (`id_historic`) REFERENCES `historics` (`id`) ON UPDATE CASCADE,
   CONSTRAINT `fk_pay_method_historics_id_pay_methods` FOREIGN KEY (`id_pay_method`) REFERENCES `pay_methods` (`id`) ON UPDATE CASCADE,
   CONSTRAINT `fk_pay_method_historics_id_users` FOREIGN KEY (`created_by`) REFERENCES `users` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci COMMENT='relation between historics and pay_methods';
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci COMMENT='relation between historics and pay_methods';
 
--- Copiando dados para a tabela fsys.pay_method_historics: ~4 rows (aproximadamente)
+-- Copiando dados para a tabela fsys.pay_method_historics: ~0 rows (aproximadamente)
 DELETE FROM `pay_method_historics`;
 /*!40000 ALTER TABLE `pay_method_historics` DISABLE KEYS */;
 /*!40000 ALTER TABLE `pay_method_historics` ENABLE KEYS */;
@@ -181,10 +202,9 @@ CREATE TABLE IF NOT EXISTS `users` (
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
--- Copiando dados para a tabela fsys.users: ~1 rows (aproximadamente)
+-- Copiando dados para a tabela fsys.users: ~0 rows (aproximadamente)
 DELETE FROM `users`;
 /*!40000 ALTER TABLE `users` DISABLE KEYS */;
-
 /*!40000 ALTER TABLE `users` ENABLE KEYS */;
 
 -- Copiando estrutura para tabela fsys.user_password_resets
@@ -212,16 +232,16 @@ CREATE TABLE IF NOT EXISTS `values` (
   `value_installment` decimal(8,2) DEFAULT NULL,
   `installments` int(11) DEFAULT NULL,
   `current_installment` int(11) DEFAULT NULL,
-  `created_by` int(11) NOT NULL DEFAULT '0',
-  `created_at` datetime DEFAULT NULL,
-  `updated_at` datetime DEFAULT NULL,
-  `deleted_at` datetime DEFAULT NULL,
+  `created_by` int(11) DEFAULT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL,
+  `deleted_at` timestamp NULL DEFAULT NULL,
   PRIMARY KEY (`id`),
   KEY `fk_values_id_users` (`created_by`),
   CONSTRAINT `fk_values_id_users` FOREIGN KEY (`created_by`) REFERENCES `users` (`id`) ON DELETE CASCADE
-) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
--- Copiando dados para a tabela fsys.values: ~4 rows (aproximadamente)
+-- Copiando dados para a tabela fsys.values: ~0 rows (aproximadamente)
 DELETE FROM `values`;
 /*!40000 ALTER TABLE `values` DISABLE KEYS */;
 /*!40000 ALTER TABLE `values` ENABLE KEYS */;
@@ -232,12 +252,13 @@ DROP VIEW IF EXISTS `v_historic`;
 CREATE TABLE `v_historic` (
 	`id` INT(11) NOT NULL,
 	`date` DATE NULL,
-	`description` VARCHAR(250) NULL COLLATE 'utf8_unicode_ci',
+	`id_description` INT(11) NULL,
 	`status` ENUM('PAID','RECEIVED','PENDING') NOT NULL COLLATE 'utf8_unicode_ci',
 	`id_value` INT(11) NULL,
 	`id_category` INT(11) NULL,
 	`category` VARCHAR(50) NOT NULL COLLATE 'utf8_unicode_ci',
 	`applicable` ENUM('IN','OUT') NOT NULL COMMENT 'IN = ENTRADA, OUT= SAIDA' COLLATE 'utf8_unicode_ci',
+	`description` VARCHAR(255) NULL COLLATE 'latin1_swedish_ci',
 	`value` DECIMAL(8,2) NOT NULL,
 	`value_installment` DECIMAL(8,2) NULL,
 	`installments` INT(11) NULL,
@@ -253,7 +274,7 @@ CREATE TABLE `v_historic` (
 -- DROP VIEW IF EXISTS `v_historic`;
 -- Removendo tabela temporária e criando a estrutura VIEW final
 DROP TABLE IF EXISTS `v_historic`;
-CREATE ALGORITHM=UNDEFINED SQL SECURITY DEFINER VIEW `v_historic` AS select `h`.`id` AS `id`,`h`.`date` AS `date`,`h`.`description` AS `description`,`h`.`status` AS `status`,`h`.`id_value` AS `id_value`,`h`.`id_category` AS `id_category`,`c`.`category` AS `category`,`c`.`applicable` AS `applicable`,`v`.`value` AS `value`,`v`.`value_installment` AS `value_installment`,`v`.`installments` AS `installments`,`v`.`current_installment` AS `current_installment`,`pm`.`pay_method` AS `pay_method`,`pm`.`applicable` AS `pm_applicable`,`pmh`.`id_pay_method` AS `id_pay_method`,`h`.`created_by` AS `created_by`,`h`.`created_at` AS `created_at` from ((((`historics` `h` join `categories` `c` on((`c`.`id` = `h`.`id_category`))) join `values` `v` on((`v`.`id` = `h`.`id_value`))) left join `pay_method_historics` `pmh` on((`pmh`.`id_historic` = `h`.`id`))) left join `pay_methods` `pm` on((`pm`.`id` = `pmh`.`id_pay_method`))) where isnull(`h`.`deleted_at`);
+CREATE ALGORITHM=UNDEFINED SQL SECURITY DEFINER VIEW `v_historic` AS select `h`.`id` AS `id`,`h`.`date` AS `date`,`h`.`id_description` AS `id_description`,`h`.`status` AS `status`,`h`.`id_value` AS `id_value`,`h`.`id_category` AS `id_category`,`c`.`category` AS `category`,`c`.`applicable` AS `applicable`,`d`.`description` AS `description`,`v`.`value` AS `value`,`v`.`value_installment` AS `value_installment`,`v`.`installments` AS `installments`,`v`.`current_installment` AS `current_installment`,`pm`.`pay_method` AS `pay_method`,`pm`.`applicable` AS `pm_applicable`,`pmh`.`id_pay_method` AS `id_pay_method`,`h`.`created_by` AS `created_by`,`h`.`created_at` AS `created_at` from (((((`historics` `h` join `categories` `c` on((`c`.`id` = `h`.`id_category`))) join `values` `v` on((`v`.`id` = `h`.`id_value`))) join `descriptions` `d` on((`d`.`id` = `h`.`id_description`))) left join `pay_method_historics` `pmh` on((`pmh`.`id_historic` = `h`.`id`))) left join `pay_methods` `pm` on((`pm`.`id` = `pmh`.`id_pay_method`))) where isnull(`h`.`deleted_at`);
 
 /*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
 /*!40014 SET FOREIGN_KEY_CHECKS=IF(@OLD_FOREIGN_KEY_CHECKS IS NULL, 1, @OLD_FOREIGN_KEY_CHECKS) */;
